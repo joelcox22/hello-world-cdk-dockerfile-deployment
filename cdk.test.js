2b3de78f1f8c357ceaf281845ec12c7adaefbf52
Joel Cox <joel@pinecodes.com>
2022-10-03 07:38:34 +1100

    chore: dont register cdk snapshot serializer manually

--- cdk.test.js
+++ cdk.test.js
@@ -1,7 +1,4 @@
 import { app } from './cdk';
-import { cdkAppSnapshotSerializer } from '@pinecodes/cdk';
-
-expect.addSnapshotSerializer(cdkAppSnapshotSerializer);
 
 test('snapshot', () => {
   expect(app).toMatchSnapshot();
